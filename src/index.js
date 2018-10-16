5ee0a8c9f8d7228135870643b7d8e80cd2ff5524
LIFLIYLI <806820876@qq.com>
2018-10-16 11:24:43 +0800

    update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
-    <h1>Hii, world!</h1>,
+    <h1>Hii我, world!</h1>,
     document.getElementById('root2')
   );
 // If you want your app to work offline and load faster, you can change
